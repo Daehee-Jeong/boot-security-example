ac123731d0cb732cc5aa94f2dcb351b1dc5e2ed2
Daehee-Jeong <dev.james.jeong@gmail.com>
2019-08-15 22:51:54 +0900

    initialize project and add gitignore file

---
+++ src/main/java/com/daehee/security/BootSecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.daehee.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootSecurityExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSecurityExampleApplication.class, args);
+    }
+
+}
